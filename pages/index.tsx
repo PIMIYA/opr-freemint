d41e4dffcca26a230e3ad4eb958d607f32ef0496
chenghsienyu <chenghsienyu@aim.com>
2023-03-17 03:52:52 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,67 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  useAddress,
+  useNetwork,
+  useNetworkMismatch,
+  ChainId,
+  Web3Button
+} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+
+  const address = useAddress();
+  const [, switchNetwork] = useNetwork();
+  const isWrongNetwork = useNetworkMismatch();
+
+  const contractAddress = "";
+
+
+  useEffect(() => {
+    if (isWrongNetwork && switchNetwork) {
+      setTimeout(() => {
+        switchNetwork(ChainId.Mumbai);
+      }, 2000);
+    }
+  }, [address, isWrongNetwork, switchNetwork]);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
+          OPR Free Mint!
         </h1>
 
         <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
+          testing OPR free mint feature.
         </p>
 
         <div className={styles.connect}>
-          <ConnectWallet />
+          <ConnectWallet
+            accentColor={isWrongNetwork ? "grey" : "navy"}
+            colorMode="dark"
+            btnTitle="Connect Wallet"
+          />
         </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
+        <div style={{ marginTop: "10px" }}>
+          {isWrongNetwork ? <p>Wrong Network, Please switchNetwork!!!</p> : <p></p>}
+          {useAddress() ? <p>Address: {address}</p> : <p></p>}
+        </div>
+        <div style={{ marginTop: "10px" }}>
+          <Web3Button
+            contractAddress={contractAddress}
+            action={(contract) => {
+              contract.call('claim', address, 1)
+            }}
+            onSuccess={() => alert("Claimed NFT!")}
+            onError={(err) => alert(err)}
           >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+            Claim NFT
+          </Web3Button>
         </div>
+
       </main>
     </div>
   );
