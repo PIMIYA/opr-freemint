25b324ea09159b8c45bf19fef83558d046edd310
chenghsienyu <chenghsienyu@aim.com>
2023-03-17 18:15:33 +0800

    update and add edition free mint feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,23 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
+const activeChain = ChainId.Mumbai;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider
+      activeChain={activeChain}
+      sdkOptions={{
+        openzeppelin: {
+          relayerUrl: "<open-zeppelin-relayer-url>", // your OZ Defender relayer URL
+          relayerForwarderAddress: "<open-zeppelin-forwarder-address>", // the OZ defender relayer address (defaults to the standard one)
+        }
+      }}
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
